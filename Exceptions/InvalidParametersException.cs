85ce462fbd687898826e1b3bf377b5226c58dd2d
rh101 <rh101@users.noreply.github.com>
2021-08-27 17:45:14 +1000

    Make Apple-specific exceptions public

--- Exceptions/InvalidParametersException.cs
+++ Exceptions/InvalidParametersException.cs
@@ -5,7 +5,7 @@ namespace AppleAuth.Exceptions
     /// <summary>
     /// Thrown when one or more parameters is null or invalid.
     /// </summary>
-    internal class InvalidParametersException : Exception
+    public class InvalidParametersException : Exception
     {
         internal InvalidParametersException(string message) : base(message) { }
     }
